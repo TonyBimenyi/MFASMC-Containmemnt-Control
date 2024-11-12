acc83e858d2be1c3324b0f596872ba33581616c3
TonyBimenyi <tonyblaisb@gmail.com>
2024-11-12 18:20:10 +0800

    initialize arrays

--- index.py
+++ index.py
@@ -23,5 +23,40 @@ L = 100
 # Generate constant desired trajectory
 yd = 0.5 * (1 + np.sign(np.sin(np.linspace(0, 2 * np.pi, L + 1))))  # Square wave
 
+# Initialize arrays
+phi1 = np.zeros((L, 1))
+phi2 = np.zeros((L, 1))
+phi3 = np.zeros((L, 1))
+phi4 = np.zeros((L, 1))
+
+mfa1 = np.zeros((L, 1))
+mfa2 = np.zeros((L, 1))
+mfa3 = np.zeros((L, 1))
+mfa4 = np.zeros((L, 1))
+
+sm1 = np.zeros((L, 1))
+sm2 = np.zeros((L, 1))
+sm3 = np.zeros((L, 1))
+sm4 = np.zeros((L, 1))
+
+u1 = np.zeros((L, 1))
+u2 = np.zeros((L, 1))
+u3 = np.zeros((L, 1))
+u4 = np.zeros((L, 1))
+
+y1 = np.zeros((L + 1, 1))
+y2 = np.zeros((L + 1, 1))
+y3 = np.zeros((L + 1, 1))
+y4 = np.zeros((L + 1, 1))
+
+e1 = np.zeros((L + 1, 1))
+e2 = np.zeros((L + 1, 1))
+e3 = np.zeros((L + 1, 1))
+e4 = np.zeros((L + 1, 1))
+
+si1 = np.zeros((L, 1))
+si2 = np.zeros((L, 1))
+si3 = np.zeros((L, 1))
+si4 = np.zeros((L, 1))
 
 
