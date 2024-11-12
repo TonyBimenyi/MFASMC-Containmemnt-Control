930ceb59bd5aa756a39b2576cfd8a9dd34e46f0c
TonyBimenyi <tonyblaisb@gmail.com>
2024-11-12 18:17:20 +0800

    ndahejeje gushiramwo parameters

--- index.py
+++ index.py
@@ -2,3 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Defining Parameters
+
+beta = -0.2
+rho = 1
+eta = 1
+lamda = 50
+mu = 1
+epsilon = 10**-5
+alpha1 = 1
+alpha2 = 1.5
+T = 0.1
+gamma1 = 0.15
+gamma2 = 0.15
+gamma3 = 0.45
+gamma4 = 0.45
+rT = 1024
+m = 350
+L = 100
+
