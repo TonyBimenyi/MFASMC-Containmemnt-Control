7c8cf7e9e61011851c159f2dc95143df5a248212
TonyBimenyi <tonyblaisb@gmail.com>
2025-05-08 15:37:21 +0800

    added parameters

---
+++ mfasmcc.m
@@ -0,0 +1,28 @@
+clc; clear;
+
+%Parameters
+
+rho = 7.5;
+eta = 1;
+lamda = 350;
+mu = 0.005;
+epsilon = 1e-5;
+alpha = 15;
+T = 0.1;
+gamma1 = 0.45;
+gamma2 = 0.15;
+gamma3 = 0.45;
+gamma4 = 0.15;
+
+beta = 10;
+sigma = 95;
+tau = 1e-5;
+
+rT = 1024;
+L = 200;
+m = 200;
+n =600;
+
+
+% initialization 
+
